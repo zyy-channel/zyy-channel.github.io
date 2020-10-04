863ca0639af1f6956d766d8ae39290a99111ba82
zhangyunyi.4dbim <zhangyunyi@4dbim.onaliyun.com>
2020-10-04 13:01:09 +0800

    padding

--- foot.js
+++ foot.js
@@ -1,3 +1,3 @@
-document.writeln("        <div class=\'jumbotron text-center\' style=\'margin-bottom:0\'>");
+document.writeln("        <div class=\'jumbotron text-center\' style=\'margin-bottom:0;padding:12\'>");
 document.writeln("            <p>我在B站学生物 张云翼</p>");
 document.writeln("        </div>");
