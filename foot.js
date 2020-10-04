32a774a6835223d71011c8370c213a28d6e7a8fe
zhangyunyi.4dbim <zhangyunyi@4dbim.onaliyun.com>
2020-10-04 12:40:36 +0800

    按钮

---
+++ foot.js
@@ -0,0 +1,3 @@
+document.writeln("        <div class=\'jumbotron text-center\' style=\'margin-bottom:0\'>");
+document.writeln("            <p>我在B站学生物 张云翼</p>");
+document.writeln("        </div>");
