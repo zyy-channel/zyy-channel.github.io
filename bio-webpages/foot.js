2e1ea05b09ca24679a7cc6798352ad4627f697df
zhangyunyi.4dbim <zhangyunyi@4dbim.onaliyun.com>
2020-10-04 21:29:25 +0800

    nav

--- bio-webpages/foot.js
+++ bio-webpages/foot.js
@@ -1,3 +1,3 @@
 document.writeln("        <div class=\'jumbotron text-center\' style=\'margin-bottom:0;padding-top:12px;padding-bottom:12px\'>");
-document.writeln("            <p>我在B站学生物 张云翼  背景音乐：www.bensound.com</p>");
+document.writeln("            <p>我在B站学生物     张云翼     背景音乐：www.bensound.com</p>");
 document.writeln("        </div>");
