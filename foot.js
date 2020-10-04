d57f17b8e93b5e9ef3c14fd8629a859e982818d6
zhangyunyi.4dbim <zhangyunyi@4dbim.onaliyun.com>
2020-10-04 13:06:23 +0800

    footer

--- foot.js
+++ foot.js
@@ -1,3 +1,3 @@
-document.writeln("        <div class=\'jumbotron text-center\' style=\'margin-bottom:0;padding:12\'>");
+document.writeln("        <div class=\'jumbotron text-center\' style=\'margin-bottom:0;padding-top:12;padding-bottom:12\'>");
 document.writeln("            <p>我在B站学生物 张云翼</p>");
 document.writeln("        </div>");
